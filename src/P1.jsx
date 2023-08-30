e163e398ca58273940e8249efa68cfd7dcc38069
Mitch <mitchell.chavarria@uao.edu.co>
2023-08-29 21:35:46 -0500

    Punto 1 del parcial(conversor)

---
+++ src/P1.jsx
@@ -0,0 +1,41 @@
+/* Primer punto de pasar segundos a Horas, minutos y segundos mostrados en la consola*/ 
+
+import { useState } from "react";
+
+export const P1 = ()=>{
+const [Horas, setHoras]= useState("")
+const [seg, setSeg]= useState("")
+
+
+
+const Conversor =()=>{
+
+    const segundo = seg
+    const segundos = (Math.round(segundo % 0x3C))
+    const horas = (Math.floor(segundo/ 0xE10))
+    const minutos = (Math.floor(segundo / 0x3C ) % 0x3C)
+    setHoras(`${horas} horas, ${minutos} minutos y ${segundos} segundos`)
+  
+    
+    console.log(Horas)
+}
+
+const ponerSeg =(e)=>{
+    setSeg(e.target.value)
+}
+return(
+    <>
+    <h1>
+     Segundos a Horas
+    </h1>
+
+    <input
+    type="number"
+    placeholder ="ingrese segundos"
+    value={seg}
+    onChange={(even)=> ponerSeg(even)}>
+    </input>
+    <button onClick={()=> Conversor()}> Convertir</button>
+    </>
+)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { P1 } from './P1.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <P1 />
   </React.StrictMode>,
 )
